883ae036a47fff3341c1c24dd50f7ac508f59877
osman goni <osman.ice.32@gmail.com>
2023-06-07 15:26:47 +0600

    absent

--- Ibos_Internship_Test/Controllers/EmployeeController.cs
+++ Ibos_Internship_Test/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using Ibos_Internship_Test.DbContexts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ibos_Internship_Test.Controllers
 {
@@ -19,10 +20,21 @@ namespace Ibos_Internship_Test.Controllers
 
         public IActionResult EmployeeSalary()
         {
-            var x =  _appDbContext.TblEmployees.Select(a => a).OrderByDescending(a => a.EmployeeSalary).ToList();
-            return Ok(x);
+            var employee_list_des =  _appDbContext.TblEmployees.Select(a => a).OrderByDescending(a => a.EmployeeSalary).ToList();
+            return Ok(employee_list_des);
         }
+        [HttpGet]
+        [Route("Employeeattendance")]
 
+        public IActionResult Employeeattendance()
+        {
+            var absent = _appDbContext.TblEmployeeAttendances
+        .Where(a => a.IsAbsent == true)
+        .Select(a => a.EmployeeId)
+        .Distinct()
+        .ToList(); 
+            return Ok(absent);
+        }
 
 
 
