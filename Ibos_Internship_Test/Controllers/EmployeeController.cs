37a2faa3b35a56ea8b1bf4d64420a6f1b5523109
osman goni <osman.ice.32@gmail.com>
2023-06-07 16:42:23 +0600

    update

--- Ibos_Internship_Test/Controllers/EmployeeController.cs
+++ Ibos_Internship_Test/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace Ibos_Internship_Test.Controllers
 {
@@ -15,6 +16,7 @@ namespace Ibos_Internship_Test.Controllers
         {
             _appDbContext = appDbContext;
         }
+
         [HttpGet]
         [Route("EmployeeSalary")]
 
@@ -23,6 +25,8 @@ namespace Ibos_Internship_Test.Controllers
             var employee_list_des =  _appDbContext.TblEmployees.Select(a => a).OrderByDescending(a => a.EmployeeSalary).ToList();
             return Ok(employee_list_des);
         }
+
+
         [HttpGet]
         [Route("Employeeattendance")]
 
@@ -34,6 +38,26 @@ namespace Ibos_Internship_Test.Controllers
         .Distinct()
         .ToList(); 
             return Ok(absent);
+        }
+
+        [HttpPut]
+        [Route("EmployeeSalary")]
+        public IActionResult EmployeeCodeUpdate(string employeeCode, long employeeId)
+        {
+            var duplicate_check_with_all_Except_own = _appDbContext.TblEmployees.Select(a => a).Where(a => a.EmployeeId != employeeId && a.EmployeeCode == employeeCode);
+
+            if (duplicate_check_with_all_Except_own.Count() > 0)
+            {
+                return BadRequest("Duplicate detected");
+            }
+            else
+            {
+                var Update_employeeCode = _appDbContext.TblEmployees.Where(z => z.EmployeeId == employeeId).FirstOrDefault();
+                Update_employeeCode.EmployeeCode = employeeCode;
+                _appDbContext.TblEmployees.Update(Update_employeeCode);
+                _appDbContext.SaveChangesAsync();
+                return Ok("Successfully done ");
+            }
         }
 
 
